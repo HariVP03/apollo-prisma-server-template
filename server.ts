35df3f7eb1263837dd992ff61144eb26c35c997d
HariVP03 <harryskotch11@gmail.com>
2022-03-24 17:25:38 +0530

    added tsconfig paths and removed .env from gitignore

--- server.ts
+++ server.ts
@@ -1,3 +1,17 @@
 import { PrismaClient } from "@prisma/client";
+import resolvers from "@resolvers";
+import typeDefs from "@typeDefs";
+import { ApolloServer } from "apollo-server";
 
 export const prisma = new PrismaClient();
+
+const server = new ApolloServer({ resolvers, typeDefs });
+
+server
+  .listen()
+  .then(({ url }) => {
+    console.log(`Server started at ${url} 🚀`);
+  })
+  .catch((e) => {
+    console.log(e);
+  });
